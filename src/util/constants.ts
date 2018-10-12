654586dc3e24c319131321b65b51e034e8c6ff4e
jodusnodus <billiet.thomas@outlook.com>
2018-10-13 00:59:43 +0200

    add mail service

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -15,7 +15,29 @@ export const MYSQL_USER = env.MYSQL_USER;
 export const MYSQL_PASSWORD = env.MYSQL_PASSWORD;
 export const MYSQL_DATABASE = env.MYSQL_DATABASE;
 
-const requiredConstants = [PORT, SESSION_SECRET, MYSQL_HOST, MYSQL_PORT, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE];
+export const MAIL_FROM = env.MAIL_FROM;
+export const MAIL_SMTP_HOST = env.MAIL_SMTP_HOST;
+export const MAIL_SMTP_PORT = parseInt(env.MAIL_SMTP_PORT);
+export const MAIL_SMTP_USERNAME = env.MAIL_SMTP_USERNAME;
+export const MAIL_SMTP_PASSWORD = env.MAIL_SMTP_PASSWORD;
+
+export const URL = env.URL;
+
+const requiredConstants = [
+    PORT,
+    SESSION_SECRET,
+    MYSQL_HOST,
+    MYSQL_PORT,
+    MYSQL_USER,
+    MYSQL_PASSWORD,
+    MYSQL_DATABASE,
+    MAIL_FROM,
+    MAIL_SMTP_HOST,
+    MAIL_SMTP_PORT,
+    MAIL_SMTP_PASSWORD,
+    MAIL_SMTP_USERNAME,
+    URL
+];
 const emptyRequiredConstants = requiredConstants.filter(x => !x);
 
 if (emptyRequiredConstants.length > 0) {
