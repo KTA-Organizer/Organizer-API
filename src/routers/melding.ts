61bb7133aabc5a16eee215d09374b4f73b396a73
VandendriesscheWard <ward.vandendriessche@student.howest.be>
2018-10-20 15:23:02 +0200

    Sending email when melding is posted added

--- src/routers/melding.ts
+++ src/routers/melding.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { check } from "express-validator/check";
+import { URL } from "../util/constants";
 import { sanitize } from "express-validator/filter";
 import executor from "./executor";
 import * as meldingenService from "../services/meldingen";
+import * as studentenService from "../services/studenten";
 import { HttpError } from "../util/httpStatus";
 import { usersOnly } from "../util/accessMiddleware";
+import * as passwordResetService from "../services/passwordReset";
+import { Opleiding } from "../models/Opleiding";
 
 const router = Router({
     mergeParams: true,
@@ -39,6 +43,10 @@ router.post("/", async function(req, res) {
             .send("Unable to add Melding, data is missing or incorrect");
         return;
     }
+    const opleidingenIds = await meldingenService.fetchOpleidingenFromMeldingAsArray(meldingId);
+    let users = await studentenService.fetchAllStudents();
+    users =  users.filter(user => opleidingenIds.indexOf(user.opleidingId) < 0);
+    await meldingenService.requestAlertMelding(users, URL + "/meldingen/" + meldingId);
     res.location("/meldingen/" + meldingId)
         .sendStatus(201);
     return;
--- src/services/meldingen.ts
+++ src/services/meldingen.ts
@@ -4,6 +4,8 @@ const knex = getKnexInstance();
 import { Melding } from "../models/Melding";
 import * as teachersService from "../services/teachers";
 import { HttpError } from "../util/httpStatus";
+import { sendMail } from "../config/mail";
+import { User } from "../models/User";
 
 async function rowToMelding(row: any) {
     if (row.teacherId) {
@@ -44,6 +46,35 @@ export async function insertMelding(meldingToAdd: any) {
     } catch (ex) {
         return;
     }
+}
+
+export async function fetchOpleidingenFromMeldingAsArray(id: number)  {
+    const rows = await knex("meldingen_opleidingen")
+        .select("opleidingId")
+        .where("meldingId", id)
+        .map((opleiding: any) => opleiding.opleidingId);
+    if (rows.length < 1)
+        return;
+    return await rows;
+}
+
+export async function requestAlertMelding(users: User[], link: string) {
+    for (const user of users) {
+        await sendNewMeldingMail(user.email, link);
+    }
+}
+
+async function sendNewMeldingMail(to: string, link: string) {
+    const html = `
+Opgelet! Eén nieuwe melding ontvangen!
+Klik<a href=${link}> hier </a>om te openen.
+  `;
+    const info: any = await sendMail({
+        to,
+        subject: "Nieuwe melding",
+        html
+    });
+    logger.info("Message sent: %s", info.messageId);
 }
 
 
