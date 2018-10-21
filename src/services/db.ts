de37c2a745efdd4ecf9684c24fe075e3fe885d07
jodusnodus <billiet.thomas@outlook.com>
2018-10-21 14:12:54 +0200

    fix db connection

--- src/services/db.ts
+++ src/services/db.ts
@@ -1,12 +1,12 @@
 import Knex from "knex";
-import { MYSQL_SOCKET, MYSQL_HOST, MYSQL_PORT, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE } from "../util/constants";
+import { INSTANCE_CONNECTION_NAME, MYSQL_HOST, MYSQL_PORT, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE } from "../util/constants";
 
 let instance: Knex;
 export default function getInstance() {
   if (!instance) {
     let connection;
-    if (MYSQL_SOCKET) {
-      connection = { socketPath: MYSQL_SOCKET};
+    if (INSTANCE_CONNECTION_NAME) {
+      connection = { socketPath: `/cloudsql/${INSTANCE_CONNECTION_NAME}`};
     } else {
       connection = {
         host: MYSQL_HOST,
