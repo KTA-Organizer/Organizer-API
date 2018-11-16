ec28bbda52a7c8389656ae1a5cecd83b6ee74bcd
jodusnodus <billiet.thomas@outlook.com>
2018-11-16 21:33:42 +0100

    finish generate and fetch report

--- src/services/evaluation.ts
+++ src/services/evaluation.ts
@@ -2,6 +2,9 @@ import { Transaction } from "knex";
 import { EvaluationSheet } from "../models/EvaluationSheet";
 import { fetchUser } from "./users";
 import { fetchFullModule } from "./modules";
+import _ from "lodash";
+import { GoalAggregateScore } from "../models/GoalScore";
+import { fetchDiscipline } from "./disciplines";
 
 
 function rowToEvaluationSheet(row: any) {
@@ -10,14 +13,20 @@ function rowToEvaluationSheet(row: any) {
 
 async function rowToFullEvaluationSheet(trx: Transaction, row: any) {
     const sheet = rowToEvaluationSheet(row);
-    const [student, teacher] = await Promise.all([
+    if (!row) {
+        return sheet;
+    }
+    const [student, teacher, scores, module] = await Promise.all([
         fetchUser(trx, sheet.studentid),
-        fetchUser(trx, sheet.teacherid)
+        fetchUser(trx, sheet.teacherid),
+        fetchScoresForEvaluationSheet(trx, sheet.id),
+        fetchFullModule(trx, sheet.moduleid)
     ]);
     sheet.student = student;
     sheet.teacher = teacher;
-    sheet.scores = await fetchScoresForEvaluationSheet(trx, sheet.id);
-    sheet.module = await fetchFullModule(trx, sheet.moduleid);
+    sheet.scores = scores;
+    sheet.module = module;
+    sheet.discipline = await fetchDiscipline(trx, module.disciplineid);
     return sheet;
 }
 
@@ -53,4 +62,29 @@ export async function insertScores(trx: Transaction, evaluationsheetid: number, 
     const rows = evaluations.map(ev => Object.assign(ev, { evaluationsheetid, creatorid }));
     await trx.table("scores").where({ evaluationsheetid }).delete();
     await trx.table("scores").insert(rows);
+}
+
+export function calculateEvaluationSheetAggregateScores(evaluationSheet: EvaluationSheet) {
+  const criteriaScores = _.groupBy(evaluationSheet.scores, "criteriaid");
+
+  const criteriaAggregates = Object.keys(criteriaScores).reduce((agg, criteriaid) => {
+    const grades = criteriaScores[criteriaid].map(s => s.grade);
+    return ({
+      ...agg,
+      [criteriaid]: _.sum(grades) / grades.length
+    });
+  }, {} as any);
+
+  const goalAggregates: GoalAggregateScore[] = _.chain(evaluationSheet.module.domains)
+    .flatMap(dom => dom.goals)
+    .map(goal => {
+        const criteriaAvgs = goal.criteria
+            .map(crit => criteriaAggregates[crit.id] * crit.weight)
+            .filter(avg => avg);
+        return { goalid: goal.id, grade: _.sum(criteriaAvgs) / criteriaAvgs.length };
+    })
+    .filter(score => score.grade)
+    .value();
+
+  return goalAggregates;
 }
